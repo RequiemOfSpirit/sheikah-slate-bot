e7eccb9526a247fc0a33affeab73194c40b96a72
RequiemOfSpirit <requiemofspirit2477@gmail.com>
2025-09-18 07:00:18 -0700

    feat(add-command-script): Allow multiple lines and quotes in resource content

--- packages/db/bin/add-command.sh
+++ packages/db/bin/add-command.sh
@@ -7,8 +7,10 @@ if [ -z "$DATABASE_URL" ]; then
 fi
 
 while true; do
-  read -p "Enter command title: " RESOURCE_TITLE
-  read -p "Enter response content for command: " RESOURCE_CONTENT
+  read -p "> Enter command title: " RESOURCE_TITLE
+
+  echo "> Enter multiline response content for command (hit Ctrl+D to finish)"
+  RESOURCE_CONTENT=$(cat | sed "s/'/''/g")
 
   # Insert into resources table
   RESOURCE_ID=$(psql "$DATABASE_URL" -t -c "INSERT INTO resources (title, content) VALUES ('$RESOURCE_TITLE', '$RESOURCE_CONTENT') RETURNING id;" | head -n 1 | xargs)
@@ -19,7 +21,7 @@ while true; do
   echo -e "\033[1;32mInserted resource with ID: $RESOURCE_ID\033[0m"
 
   while true; do
-    read -p "Enter command id/alias id: " COMMAND_NAME
+    read -p "> Enter command id/alias id: " COMMAND_NAME
 
     # Insert into commands table
     COMMAND_ID=$(psql "$DATABASE_URL" -t -c "INSERT INTO commands (name, resource_id) VALUES ('$COMMAND_NAME', '$RESOURCE_ID') RETURNING id;" | head -n 1 | xargs)
@@ -27,15 +29,15 @@ while true; do
       echo -e "\033[1;31mError: Failed to insert into commands table\033[0m"
       exit 1
     fi
-    echo -e "\033[1;32mInserted command with ID: $COMMAND_ID\033[0m\n"
+    echo -e "\033[1;32mInserted command with ID: $COMMAND_ID\033[0m"
 
-    read -p "Do you want to another command ID/alias? (y/N): " NEW_ALIAS
+    read -p "> Do you want to another command ID/alias? (y/N): " NEW_ALIAS
     if [[ "$NEW_ALIAS" != "y" && "$NEW_ALIAS" != "Y" ]]; then
       break
     fi
   done
 
-  read -p "Do you want to add another command? (y/N): " NEW_COMMAND
+  read -p "> Do you want to add another command? (y/N): " NEW_COMMAND
   if [[ "$NEW_COMMAND" != "y" && "$NEW_COMMAND" != "Y" ]]; then
     break
   fi
