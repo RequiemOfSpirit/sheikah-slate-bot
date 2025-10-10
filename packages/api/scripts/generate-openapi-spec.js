e17f258215a7214e647fbe79007cdb721e34a08e
RequiemOfSpirit <requiemofspirit2477@gmail.com>
2025-10-10 07:17:34 +0530

    chore: Bump package major versions to 1.0.0

--- packages/api/scripts/generate-openapi-spec.js
+++ packages/api/scripts/generate-openapi-spec.js
@@ -11,7 +11,7 @@ await fastify.register(fastifySwagger, {
     info: {
       title: 'Sheikah Slate Bot API',
       description: 'APIs that provide basic CRUDL operations for Sheikah Slate Resources',
-      version: '0.0.0',
+      version: '1.0.0',
     },
   },
 });
