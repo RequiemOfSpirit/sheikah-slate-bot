f9293815296d91e5a913ed582b97f89dd9c93c07
RequiemOfSpirit <requiemofspirit2477@gmail.com>
2025-12-17 15:08:45 -0800

    fix(add-command-script): Escape single quotes in resource content input

--- packages/db/bin/add-command.sh
+++ packages/db/bin/add-command.sh
@@ -9,6 +9,7 @@ fi
 while true; do
   read -p "> Enter command title: " RESOURCE_TITLE
   read -p "> Enter response content for command (no newlines): " RESOURCE_CONTENT
+  RESOURCE_CONTENT=$(echo "$RESOURCE_CONTENT" | sed "s/'/''/g") # Escape single quotes for SQL
 
   # Insert into resources table
   RESOURCE_ID=$(psql "$DATABASE_URL" -t -c "INSERT INTO resources (title, content) VALUES ('$RESOURCE_TITLE', '$RESOURCE_CONTENT') RETURNING id;" | head -n 1 | xargs)
